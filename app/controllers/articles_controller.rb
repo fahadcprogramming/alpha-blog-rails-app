203e35ac7340c21645500abdc11265a0e7ecfd98
Rails MH course <railsudemycourse@gmail.com>
2018-03-17 02:18:21 +0000

    Edit, update, show and index added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
   class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all
+  end  
+  
   def new
     @article = Article.new
   end
@@ -11,15 +15,27 @@
       redirect_to article_path(@article)
     else
       render 'new'
-    end  
-    
-    
+    end
   end
   
   def show
     @article = Article.find(params[:id])
   end  
-    
+  
+  def edit
+    @article = Article.find(params[:id])
+  end  
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article updated successfully."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end  
+  end
+  
   private
     def article_params
       puts "article_params method"
